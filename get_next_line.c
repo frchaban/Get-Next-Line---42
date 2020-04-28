599c4fe523f273154ebaea8c273a892bd4b2feca
Francois Chabanne <fe.chabanne@gmail.com>
2020-04-28 18:54:13 +0200

    WIP

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: frchaban <frchaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/27 10:34:40 by frchaban          #+#    #+#             */
-/*   Updated: 2020/04/28 18:22:13 by frchaban         ###   ########.fr       */
+/*   Updated: 2020/04/28 18:51:47 by frchaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_set_line(char const *str, char *line)
 	unsigned int	j;
 
 	i = 0;
+    if (!str)
+        return (NULL);
 	while (str[i] && str[i] != '\n')
 		i++;
 	if (!(line = (char *)malloc(sizeof(*line) * (i + 1))))
@@ -48,11 +50,8 @@ char	*ft_read(int fd, char *str, int *flag)
 			return (str);
 	}
     if (rd == 0)
-    {
         *flag = 1;
-        return (str);
-    }
-	if (rd == -1)
+	else if (rd == -1)
 		return (NULL);
 	return (str);
 }
@@ -62,37 +61,23 @@ int		get_next_line(int fd, char **line)
 	static char *str;
     int         flag;
 
+    flag = 0;
     if (fd < 0 || line == NULL)
         return (-1);
-	if (str && str[0] != '\0' && ft_strchr(str, '\n'))
-	{
-		*line = ft_set_line(str, *line);
-		str = ft_substr(str, ft_strlen(*line) + 1, ft_strlen(str));
-		if (str && str[0] == '\0')
-        {
-			free(str);
-            str = NULL;
-        }
-		return (1);
-	}
-	else if ((str = ft_read(fd, str, &flag)))
-	{
-		*line = ft_set_line(str, *line);
-		str = ft_substr(str, ft_strlen(*line) + 1, ft_strlen(str));
-		if (str && str[0] == '\0')
-        {
-			free(str);
-            str = NULL;
-        }
-        if (flag == 1)
-            return (0);
-		return (1);
-	}
-    if (fd == 0 && flag == 1)
-        return (0);
-    if (flag == 1 && str)
-            return (0);
-	if (str == NULL)
+	if (!(str && ft_strchr(str, '\n')))
+        str = ft_read(fd, str, &flag);
+    if (str == NULL && fd != 0)
 		return (-1);
+	*line = ft_set_line(str, *line);
+	str = ft_substr(str, ft_strlen(*line) + 1, ft_strlen(str));
+    if (str && str[0] == '\0')
+    {
+		free(str);
+        str = NULL;
+    }
+    if ((fd == 0 && flag == 1) || (flag == 1 && str))
+        return (0);
+    if (flag == 0)
+		return (1);
 	return (0);
 }
